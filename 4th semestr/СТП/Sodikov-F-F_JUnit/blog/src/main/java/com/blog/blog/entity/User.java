e23af60118970fe9ecc4f1fdfc4e6fecae8b4691
Farkhad <70976570+farkhads21@users.noreply.github.com>
2021-05-27 12:40:01 +0300

    Add files via upload

---
+++ 4th semestr/СТП/Sodikov-F-F_JUnit/blog/src/main/java/com/blog/blog/entity/User.java
@@ -0,0 +1,31 @@
+package com.blog.blog.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "t_user")
+
+public class User {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+
+        @Column(unique = true)
+        private String login;
+
+        @Column(unique = true)
+        private String email;
+
+        private String password;
+
+        private boolean isActivate;
+
+        @OneToMany(mappedBy = "author")
+        private Set<Post> posts;
+}
